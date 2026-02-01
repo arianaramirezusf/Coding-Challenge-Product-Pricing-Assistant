50eaeabdf42ab8bf86d8bde1f6ff3e38841a6808
arianaramirezusf <arianaramirez@usf.edu>
2026-02-01 14:21:12 -0500

    Connecting the HTML to the JS file

--- pricingassistant.js
+++ pricingassistant.js
@@ -1,11 +1,26 @@
+let productName = "Headphones";
+
 let costPerUnit = 12;
+
 let basePrice = 22.99;
+
 let discountRate = 0.25;
+
 let salesTaxRate = 0.07;
+
 let fixedMonthlyCosts = 5200;
+
 
 discountedPrice = basePrice * (1 - discountRate);
 finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
 profitPerUnit = finalPriceWithTax - costPerUnit;
 breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
 isProfitablePerUnit = profitPerUnit > 0;
+
+
+console.log("productName");
+console.log("discountedPrice");
+console.log("finalPriceWithTax");
+console.log("profitPerUnit");
+console.log("breakEvenUnits");
+console.log("isProfitablePerUnit");
