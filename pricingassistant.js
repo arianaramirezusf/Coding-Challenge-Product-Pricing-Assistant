ae95dcae151e0ccb321be765fbed866497b2b249
arianaramirezusf <arianaramirez@usf.edu>
2026-02-01 13:45:27 -0500

    Calculating Variables and Metrics

---
+++ pricingassistant.js
@@ -0,0 +1,11 @@
+let costPerUnit = 12;
+let basePrice = 22.99;
+let discountRate = 0.25;
+let salesTaxRate = 0.07;
+let fixedMonthlyCosts = 5200;
+
+discountedPrice = basePrice * (1 - discountRate);
+finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
+profitPerUnit = finalPriceWithTax - costPerUnit;
+breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
+isProfitablePerUnit = profitPerUnit > 0;
